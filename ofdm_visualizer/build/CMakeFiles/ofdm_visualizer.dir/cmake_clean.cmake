a0b3a48619feeece6a5502e0ede18824248afbab
Gaba5454 <egor.shvarev@bk.ru>
2026-03-23 23:28:27 +0700

    First commit

---
+++ ofdm_visualizer/build/CMakeFiles/ofdm_visualizer.dir/cmake_clean.cmake
@@ -0,0 +1,31 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ofdm_visualizer.dir/imgui/backends/imgui_impl_glfw.cpp.o"
+  "CMakeFiles/ofdm_visualizer.dir/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "CMakeFiles/ofdm_visualizer.dir/imgui/backends/imgui_impl_opengl3.cpp.o"
+  "CMakeFiles/ofdm_visualizer.dir/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "CMakeFiles/ofdm_visualizer.dir/imgui/imgui.cpp.o"
+  "CMakeFiles/ofdm_visualizer.dir/imgui/imgui.cpp.o.d"
+  "CMakeFiles/ofdm_visualizer.dir/imgui/imgui_demo.cpp.o"
+  "CMakeFiles/ofdm_visualizer.dir/imgui/imgui_demo.cpp.o.d"
+  "CMakeFiles/ofdm_visualizer.dir/imgui/imgui_draw.cpp.o"
+  "CMakeFiles/ofdm_visualizer.dir/imgui/imgui_draw.cpp.o.d"
+  "CMakeFiles/ofdm_visualizer.dir/imgui/imgui_tables.cpp.o"
+  "CMakeFiles/ofdm_visualizer.dir/imgui/imgui_tables.cpp.o.d"
+  "CMakeFiles/ofdm_visualizer.dir/imgui/imgui_widgets.cpp.o"
+  "CMakeFiles/ofdm_visualizer.dir/imgui/imgui_widgets.cpp.o.d"
+  "CMakeFiles/ofdm_visualizer.dir/implot/implot.cpp.o"
+  "CMakeFiles/ofdm_visualizer.dir/implot/implot.cpp.o.d"
+  "CMakeFiles/ofdm_visualizer.dir/implot/implot_demo.cpp.o"
+  "CMakeFiles/ofdm_visualizer.dir/implot/implot_demo.cpp.o.d"
+  "CMakeFiles/ofdm_visualizer.dir/implot/implot_items.cpp.o"
+  "CMakeFiles/ofdm_visualizer.dir/implot/implot_items.cpp.o.d"
+  "CMakeFiles/ofdm_visualizer.dir/main.cpp.o"
+  "CMakeFiles/ofdm_visualizer.dir/main.cpp.o.d"
+  "ofdm_visualizer"
+  "ofdm_visualizer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ofdm_visualizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
