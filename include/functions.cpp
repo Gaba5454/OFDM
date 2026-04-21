ba116adf4a65a2e62cf7e85c733e168184f79e30
Egor <egor.shvarev@bk.ru>
2026-04-21 16:20:55 +0700

    Add ZadoffChu and PSS configuration functions

--- include/functions.cpp
+++ include/functions.cpp
@@ -16,13 +16,13 @@ std::vector<int8_t> string_to_bits(const std::string& text) {
 
 
 // Думаю нужно поменять алгоритм работы этой функции, чтобы она была более C подобной
-std::vector<ComplexSymbol> cyclicPrefix(const std::vector<ComplexSymbol>& symbol, size_t cp_len) {
+std::vector<CD> cyclicPrefix(const std::vector<CD>& symbol, size_t cp_len) {
 
     if(cp_len >= symbol.size()){
         throw std::invalid_argument("Длина должна быть меньше размера символа.");
     }
 
-    std::vector<ComplexSymbol> output;
+    std::vector<CD> output;
 
     output.reserve(symbol.size()+cp_len);
 
@@ -46,7 +46,7 @@ std::vector<ComplexSymbol> cyclicPrefix(const std::vector<ComplexSymbol>& symbol
 @param arr_len размер входного массива
 @return arr_channel массив данных после обработки
 */
-std::vector<ComplexSymbol> channelSimulation(const std::vector<ComplexSymbol>& arrayForTx, size_t arr_len, double noise_stddev = 1.0) {
+std::vector<CD> channelSimulation(const std::vector<CD>& arrayForTx, size_t arr_len, double noise_stddev = 1.0) {
     
     size_t length = arr_len + arr_len;
     fftw_complex* arr_channel = (fftw_complex*)fftw_malloc(sizeof(fftw_complex) * length);
@@ -66,15 +66,48 @@ std::vector<ComplexSymbol> channelSimulation(const std::vector<ComplexSymbol>& a
         arr_channel[i][0] = dist(gen); // Действительная часть (Re)
         arr_channel[i][1] = dist(gen); // Мнимая часть (Im)
     }
-    std::vector<ComplexSymbol> result;
+    std::vector<CD> result;
     result.reserve(length);
         for (size_t i = 0; i < length; ++i) {
-        // Создаём ComplexSymbol из действительной и мнимой части
-        // Если ComplexSymbol — это std::complex<double>, этот код тоже сработает
+        // Создаём CD из действительной и мнимой части
+        // Если CD — это std::complex<double>, этот код тоже сработает
         result.emplace_back(arr_channel[i][0], arr_channel[i][1]);
     }
      // 2. Освобождение памяти FFTW (обязательно!)
     fftw_free(arr_channel);
 
     return result;
+}
+
+std::vector<CD> ZadoffChu(int U){
+    std::vector<CD> d(62);
+    double epsilon = 0.001;
+
+    for(size_t i = 0; i < 30; i++){
+        d[i] = exp((-j * M_PI * CD(U) * CD(i) * CD(i+1)) / CD(63));
+        if(std::abs(imag(d[i]) < epsilon)) { d[i] = CD(real(d[i]),0); }
+    }
+    for(size_t i = 31; i < 61; i++){
+        d[i] = exp((-j * M_PI * CD(U) * CD(i+1) * CD(i+2)) / CD(63));
+        if(std::abs(imag(d[i]) < epsilon)) { d[i] = CD(real(d[i]),0); }
+    }
+    return d;
+}
+
+/*
+* NID - PSS type
+*/
+std::vector<CD> PSS(size_t NID) {
+    std::vector<CD> pssArr;
+    if(NID == 0) {
+        pssArr = ZadoffChu(25);
+    }
+    else if (NID == 1) {
+        pssArr = ZadoffChu(29);
+    }
+    else if (NID == 2) {
+        pssArr = ZadoffChu(34);
+    }
+
+    return pssArr;
 }
