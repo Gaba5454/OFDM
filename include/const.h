5b51338d5f51bc27a829032a815e1b6f99b3483b
Egor <egor.shvarev@bk.ru>
2026-04-21 12:15:55 +0700

    Add file with constants

---
+++ include/const.h
@@ -0,0 +1,16 @@
+#ifndef CONST_H
+#define CONST_H
+#include <iostream>
+#include <fftw3.h>
+#include <math.h>
+#include <complex>
+#include <vector>
+#include <bitset>
+#include <fstream>
+#include <random> 
+#include <cstdint>
+
+constexpr uint8_t LTE = 128;
+
+using ComplexSymbol = std::complex<double>;
+#endif
