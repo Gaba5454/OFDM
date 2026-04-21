019fd3ee97d6d4d758a2d7b0ea1e3815546b6c10
Egor <egor.shvarev@bk.ru>
2026-04-21 12:17:16 +0700

    Add file for modulation function

---
+++ include/modulations.cpp
@@ -0,0 +1,28 @@
+// modulations.cpp
+#include "const.h"
+
+std::vector<ComplexSymbol> QPSK(const std::vector<int8_t> &in_bits) {
+    
+// Переменная количество выходных символов
+    size_t num_symbols = in_bits.size() / 2;
+
+// Объявляется переменная для выходного массива, и сразу же удобно 
+// под неё выделяется память на количество выходных символов
+    std::vector<ComplexSymbol> out_sym;
+    out_sym.reserve(num_symbols);
+
+// Цикл преобразования битов в символы, в конце каждой итерации
+// Вычисляется финальная координата точки (с нормализацией).
+// Конструируется объект std::complex прямо в памяти вектора.
+// Увеличивается размер вектора на 1.
+    for(size_t i = 0; i < in_bits.size(); i+=2) {
+        int8_t bit0 = in_bits[i];
+        int8_t bit1 = in_bits[i+1];
+
+        double i_component = (bit0 == 0) ? -1.0f : 1.0f;
+        double q_component = (bit1 == 0) ? -1.0f : 1.0f;
+
+        out_sym.emplace_back(i_component, q_component);
+    }
+    return out_sym;
+}
---
+++ include/modulations.h
@@ -0,0 +1,6 @@
+#ifndef MODULATIONS_H
+#define MODULATIONS_H
+
+std::vector<ComplexSymbol> QPSK(const std::vector<int8_t> &in_bits);
+
+#endif
