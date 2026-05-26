c8514bffbaaaf4abf8e224b3093b76fcd8f6ccdf
Gaba5454 <egor.shvarev@bk.ru>
2026-05-26 12:26:46 +0700

    RX part

--- include/receive.cpp
+++ include/receive.cpp
@@ -1,10 +1,4 @@
-#include <SoapySDR/Device.h>
-#include <SoapySDR/Formats.h>
-#include <iostream>
-#include <stdlib.h>
-#include <math.h>
-#include <vector>
-#include "const.h"
+#include "receive.h"
 
 
 void ModeRX(SoapySDRDevice *sdr, std::vector<CF>& tx_array, size_t iteration_count) {
