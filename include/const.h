6a7cdb3b23ab50af580e38262ecaf11625d3e171
Gaba5454 <egor.shvarev@bk.ru>
2026-05-26 12:23:16 +0700

    Constant arguments

--- include/const.h
+++ include/const.h
@@ -1,28 +1,24 @@
-// const.h
-#ifndef CONST_H
-#define CONST_H
+#pragma once
+
 #include <iostream>
-#include <fftw3.h>
-#include <math.h>
 #include <complex>
 #include <vector>
-#include <bitset>
-#include <fstream>
-#include <random> 
 #include <cstdint>
-#include <algorithm>
+#include <cstddef>  
 
-static const std::complex<float> j(0.0, 1.0);
+constexpr std::complex<float> j(0.0, 1.0);
 
 constexpr size_t LTE = 128;
-const size_t CP_LENGTH = 20;  // Length of the CP ~1/12 on 128 symbols
+constexpr size_t CP_LENGTH = 20;  // Length of the CP ~1/12 on 128 symbols
+constexpr size_t SYMBOL_LEN = LTE + CP_LENGTH; // 148
+
 using CF = std::complex<float>;
-const size_t iter = 10; 
+constexpr size_t iter = 10; 
 struct DecodedResult {
     std::string recovered_text;
     std::vector<CF> constellation_points; // Для визуализации созвездия всех символов
     size_t symbols_processed;
 };
-//using VCD = std::complex<CF>;
 
-#endif
+constexpr double QPSK_LOW = -1.0;
+constexpr double QPSK_HIGH = +1.0;
